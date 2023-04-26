08aa2554b73f90ed7a56ede5c5f460ec339633dc
shrayanchummu <shrayanchummu@gmail.com>
2023-04-27 01:52:46 +0530

    Successfully sidebar functionaility added

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ console.log("HELLO HELLO")
 document.querySelector('.cross').style.display ='none';
 document.querySelector('.ham').addEventListener("click",()=>{
     document.querySelector('.sidebar').classList.toggle('sidebarGo');
+    if(document.querySelector('.sidebar').classList.contains('sidebarGo'))
+    {
+        document.querySelector('.bar').style.display ='inline';
+        document.querySelector('.cross').style.display ='none';
+    }
+    else
+    {
+        document.querySelector('.bar').style.display ='none';
+        setTimeout(()=>{
+            document.querySelector('.cross').style.display ='inline';
+        },300)
+        
+    }
 }) 
 
 
